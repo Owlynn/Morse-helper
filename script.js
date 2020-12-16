20860dcbe70a3d2f993650b631f7ad8d6a963feb
Owlynn <contact@jessalynn.fr>
2020-12-16 15:42:21 +0100

    dynamic keyboard creation

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let keyboard = {
-    letter:,
+    alphabet : ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
     createKey () {
-        
+        for(i = 0 ; i< keyboard.alphabet.length ; i++) {
+            let singleKey = document.createElement("div");
+            document.getElementById('alphabet').appendChild(singleKey);
+            singleKey.classList.add("keyboard-letter");
+            singleKey.innerHTML= keyboard.alphabet[i];
+        }
+      
     }
 }
+
+keyboard.createKey();
