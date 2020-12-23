fb95a4d237f80da47435450b199d7da5f3bf99de
Owlynn <contact@jessalynn.fr>
2020-12-23 16:57:04 +0100

    bon, maintenant au moins il jou ele bon premeir son.

--- script.js
+++ script.js
@@ -20,39 +20,41 @@ let visualMorse = {
     morseAlphabet : ["._","_...","_._.","_..",".",".._.","__.","....","..",".___","_._","._..","__","_.","___",".__.","__._","._.","...","_",".._","..._",".__","_.._","_.__","__.."],
     letterDisplayed : document.createElement('div'),
     morseDisplayed : document.createElement('div'),
-
+    
     displayLetter(event){
+        
         document.querySelector(".display-letter").innerHTML = "";
         visualMorse.letterDisplayed = document.createElement('div');
         document.querySelector(".display-letter").appendChild(visualMorse.letterDisplayed);
         visualMorse.letterDisplayed.classList.add("letter-displayed");
         visualMorse.letterDisplayed.innerHTML = event.target.innerHTML;
-        audioMorse.combineSounds();
         
         visualMorse.morseDisplayed = document.createElement('div');
         document.querySelector(".display-letter").appendChild(visualMorse.morseDisplayed);
         visualMorse.morseDisplayed.classList.add("visual-morse-displayed");
         let index = keyboard.alphabet.findIndex(letter => letter === event.target.innerHTML);
         visualMorse.morseDisplayed.innerHTML = visualMorse.morseAlphabet[index].split('').join(' ');
+        
+        audioMorse.combineSounds();
     }
 };
 let audioMorse = {
-        long : document.getElementById("long"),
-        short : document.getElementById("short"),
-        combineSounds(){
-            audioMorse.short.currentTime = 0;
-            audioMorse.long.currentTime = 0;
-            console.log("combine");
-            let splittedMorse = visualMorse.morseDisplayed.innerHTML.split(' ');
-            console.log(splittedMorse[0]);
-            if (splittedMorse[0] == '.') {
-                audioMorse.short.play();
-            }else{
-                audioMorse.long.play();
-            }
+    long : document.getElementById("long"),
+    short : document.getElementById("short"),
+    combineSounds(){
+        audioMorse.short.currentTime = 0;
+        audioMorse.long.currentTime = 0;
+        console.log("combine");
+        let splittedMorse = visualMorse.morseDisplayed.innerHTML.split(' ');
+        console.log(splittedMorse[0]);
+        if (splittedMorse[0] == '.') {
+            audioMorse.short.play();
+        }else{
+            audioMorse.long.play();
         }
+    }
 };
-        
+
 let app = {
     init(){
         keyboard.createKeyboard();
